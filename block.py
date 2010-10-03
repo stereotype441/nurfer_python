74afa801701d7c4a2c206668515891c4bc10712e
Paul Berry <stereotype441@gmail.com>
2010-10-02 17:56:48 -0700

    Added tests to block.py

--- block.py
+++ block.py
@@ -48,13 +48,29 @@ class Block(object):
         return bound_names
 
 
+class TestBlock(unittest.TestCase):
+    @staticmethod
+    def mk_line(text):
+        return LogicalLine(text, 0, len(text), 0)
+
+    def test_find_bound_names(self):
+        self.assertEqual(Block(None, [self.mk_line('a = 7')]).find_bound_names(), set(['a']))
+        self.assertEqual(Block(None, [self.mk_line('x = y')]).find_bound_names(), set(['x']))
+        def_f_x_y = Block(self.mk_line('def f(x, y):'), [self.mk_line('    return y, z')])
+        self.assertEqual(Block(None, [def_f_x_y]).find_bound_names(), set(['f']))
+        self.assertEqual(def_f_x_y.find_bound_names(), set(['x', 'y']))
+        class_myclass = Block(self.mk_line('class MyClass(foo):'), [self.mk_line('    x = 5')])
+        self.assertEqual(Block(None, [class_myclass]).find_bound_names(), set(['MyClass']))
+        self.assertEqual(class_myclass.find_bound_names(), set(['x']))
+
+
 class TestFindBoundNames(unittest.TestCase):
     def test_find_bound_names(self):
         test_cases = [('x = y', ['x']),
-                      ('def f(x, y)\n    return y, z', ['f']),
+                      ('def f(x, y):\n    return y, z', ['f']),
                       ('z = "foo"\ndef f(x, y)\n    return y, z', ['z', 'f']),
-                      ('def f(x, y)\n    return y.z', ['f']),
-                      ('def f(x, y)\n    z = x\n    return (y, z)', ['f']),
+                      ('def f(x, y):\n    return y.z', ['f']),
+                      ('def f(x, y):\n    z = x\n    return (y, z)', ['f']),
                       ('def f():\n    print "f"\nf()', ['f']),
                       ('class MyClass(object):\n    pass\nprint MyClass()', ['MyClass']),
                       ('class MyClass(foo):\n    pass\nprint MyClass()', ['MyClass'])]
